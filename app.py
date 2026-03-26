9f5d7cbc34832ecc2c1b82777f4b005d3d943f21
Uttara Mekala <uttarachoudary@gmail.com>
2026-03-26 15:12:50 -0400

    Fix flake8 formatting issues

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 def add(a, b):
     return a + b
 
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
-    print(add(2, 3))
+    print("Add:", add(2, 3))
+    print("Subtract:", subtract(5, 2))
+    print("Multiply:", multiply(4, 3))
